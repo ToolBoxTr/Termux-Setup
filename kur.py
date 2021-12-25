aadeda607669c726aa992bef26d0cdf4108f8e08
ToolBoxTr <96628333+ToolBoxTr@users.noreply.github.com>
2021-12-25 19:38:23 +0300

    kur.py

---
+++ kur.py
@@ -0,0 +1,32 @@
+#!/use/bin/env python3
+
+import os
+os.system("pkg  upgrade && pkg uprage -y")
+os.system("termux-setup-storage")
+os.system("pkg up -y")
+os.system("pkg install curl -y")
+os.system("pkg install wget -y")
+
+os.system("pkg install python2 -y")
+os.system("pkg install python3 -y")
+os.system("pkg install php -y")
+os.system("pkg install perl -y")
+os.system("pkg install nano -y")
+os.system("pkg install vim -y")
+os.system("pkg install cat -y")
+os.system("pkg install pip -y")
+os.system("pkg install pip2 -y")
+os.system("pkg install wordlist -y")
+os.system("pkg install nmap -y")
+os.system("pkg install hydra -y")
+os.system("pkg install openssl")
+os.system("apt install nodejs")
+os.system("pkg install wim")
+os.system("pip install colorama")
+os.system("pip install requests")
+
+os.system("termux-open-url http://www.toolboxtr.com")
+
+
+os.system("clear")
+print("KURULUM BİTTİ")
